8ab60d443486e39ecd72d71b449aac5c6be96320
S Sanjith <sanji123444@gmail.com>
2022-08-03 01:06:32 +0530

    Search Bar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,36 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import './App.css';
+import TextField from "@mui/material/TextField";
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from "@material-ui/core/IconButton";
 
 function App() {
   return (
     <div className='app'>
-      <form>
-        <label for='searchField'>Enter anything:  </label>
-        <input type='text' id='searchField'></input>
-      </form>
+      <SearchBar />
     </div>
+  )
+}
+
+function SearchBar() {
+  return (
+  <div className='search'>
+      <h2>Search video...</h2>
+      <TextField
+        fullWidth
+        label="Search"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment>
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          )
+        }}
+      />
+  </div>
   )
 }
 
